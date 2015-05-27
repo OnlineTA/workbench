813927df88325a1f73404cf81d2f6e8377cebdd0
Oleksandr Shturmov <oleks@oleks.info>
2015-05-27 19:10:11 +0200

    compiling code, now with more TODOs

--- client.go
+++ client.go
@@ -11,11 +11,12 @@ func mktmp() string {
   var tmpdir string
   var err error
 
-  tmpdir, err = ioutil.TempDir(os.TempDir, "workbench")
+  tmpdir, err = ioutil.TempDir(os.TempDir(), "workbench")
   if err != nil {
     if os.IsExist(err) {
       log.Fatalf("Too many temp directories for Go to handle.")
     }
+    // TODO: maybe other error conditions (like out-of-space)
   }
 
   return tmpdir
@@ -28,6 +29,8 @@ func rmtmp(tmpdir string) {
   if err != nil {
     log.Fatalf("Couldn't remove %s.", tmpdir)
   }
+
+  // TODO: err is redundant, but we should be shreding anyhow..
 }
 
 func client(assignment string, submission string) {
