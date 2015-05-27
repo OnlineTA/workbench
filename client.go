13bdc140500bca341eeb4b888ab5578779ecb885
Oleksandr Shturmov <oleks@oleks.info>
2015-05-27 18:40:16 +0200

    intitial commit

---
+++ client.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+  "fmt"
+)
+
+func client(assignment string, submission string) {
+  fmt.Println("HEJ!")
+}
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+  "flag"
+  "fmt"
+  "os"
+)
+
+func usage() {
+  fmt.Fprintf(os.Stderr,
+    "Usage: %s ASSIGNMENT SUBMISSION\n", os.Args[0])
+}
+
+func main() {
+  var isServer bool
+  var assignment string
+  var submission string
+
+  flag.Usage = usage
+  flag.BoolVar(&isServer, "server", false, "run in server mode")
+  flag.Parse()
+
+  args := flag.Args()
+  if len(args) != 2 {
+    flag.Usage()
+    os.Exit(1)
+  }
+
+  assignment = args[0]
+  submission = args[1]
+
+  if ! isServer {
+    client(assignment, submission)
+  }
+}
