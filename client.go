ef642fbbb8b1f384550b22caf00c5cdf93eac58a
Oleksandr Shturmov <oleks@oleks.info>
2015-05-27 19:04:04 +0200

    create tmpdir and remove it afterwards

--- client.go
+++ client.go
@@ -2,8 +2,39 @@ package main
 
 import (
   "fmt"
+  "io/ioutil"
+  "log"
+  "os"
 )
 
+func mktmp() string {
+  var tmpdir string
+  var err error
+
+  tmpdir, err = ioutil.TempDir(".", "workbench")
+  if err != nil {
+    if os.IsExist(err) {
+      log.Fatalf("Too many temp directories for Go to handle.")
+    }
+  }
+
+  return tmpdir
+}
+
+func rmtmp(tmpdir string) {
+  var err error
+
+  err = os.RemoveAll(tmpdir) // TODO: shred
+  if err != nil {
+    log.Fatalf("Couldn't remove %s.", tmpdir)
+  }
+}
+
 func client(assignment string, submission string) {
+  var tmpdir string
+
+  tmpdir = mktmp()
+  defer rmtmp(tmpdir)
+
   fmt.Println("HEJ!")
 }
