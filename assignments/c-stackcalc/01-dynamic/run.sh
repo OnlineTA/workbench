9553c8a1ea382d24c3a81fb9e65f47c270d3ce19
Oleksandr Shturmov <oleks@oleks.info>
2015-06-02 12:24:38 +0200

    check exit code on exit

--- assignments/c-stackcalc/01-dynamic/run.sh
+++ assignments/c-stackcalc/01-dynamic/run.sh
@@ -5,9 +5,12 @@ cd "$1"
 ./main <<EOF
 5
 5
-+
-p
-p
+0
 EOF
 
-echo $?
+EXIT_CODE="$?"
+if [ "$EXIT_CODE" != 0 ] ; then
+  echo "Din lommeregner afsluttede med kode $EXIT_CODE."
+  echo "Vi forventede at den afsluttede med kode 0."
+  exit 2
+fi
